e0edb00b58ff59bb7824e57564a53e6bb56da3ab
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-03-12 02:46:21 +0600

    1

---
+++ eventloop-prac.js
@@ -0,0 +1,12 @@
+console.log('rakib');
+setTimeout(function setTimeout() {
+    console.log('rakib: First e ami');
+}, 2000)
+console.log('fahim');
+setTimeout(function setTimeout() {
+    console.log('fahim: second e ami');
+}, 3000)
+console.log('hridoy');
+setTimeout(function setTimeout() {
+    console.log('hridoy: last e ami');
+}, 4000)
